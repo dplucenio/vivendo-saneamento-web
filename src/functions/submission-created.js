62df6760168192338556f931c380aadcef4ecd0f
Daniel Plucenio <dplucenio@gmail.com>
2020-06-11 11:16:37 -0300

    wip

--- src/functions/submission-created.js
+++ src/functions/submission-created.js
@@ -1,21 +1,20 @@
+const dotenv = require('dotenv');
+const axios = require('axios');
+dotenv.config();
 
 const hooks = {
-  newsletterForm() {
+  newsletterForm(data, callback) {
     console.log('execute when newsletterForm');
+    let response = { statusCode: 200, body: "hello functions" };
+    callback(null, response);
   }
 }
 
 
 function submissionCreated(event, context, callback) {
-  console.log(JSON.stringify(event));
-  console.log(JSON.stringify(context));
-  console.log(JSON.stringify(event.body));
-  const formName = JSON.parse(event.body).payload.form_name;
-  console.log(formName);
-  const hook = hooks[formName];
-  hook();
-  let response = {statusCode: 200, body: "hello functions"};
-  callback(null, response);
+  const payload = JSON.parse(event.body).payload;
+  const hook = hooks[payload.formName];
+  return hook(payload.data, callback);
 }
 
 module.exports = {
