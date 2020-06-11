e85e0df203a27c0199da6b87cb85d54b86685fd0
Daniel Plucenio <dplucenio@gmail.com>
2020-06-11 10:52:29 -0300

    wip

--- src/functions/submission-created.js
+++ src/functions/submission-created.js
@@ -2,6 +2,7 @@ function submissionCreated(event, context, callback) {
   console.log(JSON.stringify(event));
   console.log(JSON.stringify(context));
   console.log(JSON.stringify(event.body));
+  console.log(event.body.payload.form_name);
   let response = {statusCode: 200, body: "hello functions"};
   callback(null, response);
 }
