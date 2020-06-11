2a5c100dcb82bafe96b04fbf0f204bc498f5c0c1
Daniel Plucenio <dplucenio@gmail.com>
2020-06-11 10:56:25 -0300

    wip

--- src/functions/submission-created.js
+++ src/functions/submission-created.js
@@ -1,8 +1,18 @@
+
+const hooks = {
+  newsletterForm() {
+    console.log('execute when newsletterForm');
+  }
+}
+
+
 function submissionCreated(event, context, callback) {
   console.log(JSON.stringify(event));
   console.log(JSON.stringify(context));
   console.log(JSON.stringify(event.body));
   console.log(event.body.payload.form_name);
+  let hook = hooks[event.body.payload.form_name];
+  hook();
   let response = {statusCode: 200, body: "hello functions"};
   callback(null, response);
 }
